a1e7dfb6cc3ab7c82abd32d6c220ffafa1815f38
Nitish8800 <nitishfea120784@gmail.com>
2022-07-02 12:48:05 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -11,11 +12,24 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
-app.get("/", (request, response) => {
-  response.send("API is Running Sucessfully ");
-});
-
 app.use("/api/user", userRoutes);
+
+// ------------------  Deploy ment           ----------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "productions") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (request, response) => {
+    response.send("API is Running Sucessfully ");
+  });
+}
+// ------------------  Deploy ment           ----------------------
 
 // Error Handling middlewares
 app.use(notFound);
